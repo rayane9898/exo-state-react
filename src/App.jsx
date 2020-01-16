5eb2e0474fa12ee664054d74db49cb424380ece6
rayane <rayanetahiriben@icloud.com>
2020-01-16 12:03:30 +0100

    date fini mais incomplète

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Date from "./compenents/Date"
 
 
 function App() {
   return (
     <div>
-
+      <Date />
     </div>
 
   )
---
+++ src/compenents/Date.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+
+
+
+const getTimes = () => {
+
+    
+    const laDate = new Date();
+   
+
+    console.log(laDate);
+    
+
+    
+    return(
+        <div>
+            <p>Il est {laDate}</p>
+        </div>
+
+    )
+   
+}
+
+export default Date  
